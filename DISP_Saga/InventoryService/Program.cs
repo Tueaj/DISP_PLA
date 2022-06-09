84fb5e0fc9b4271a1e759b5921baed812affc48a
Tue Alexander Jørgensen <tue.alexander.j@gmail.com>
2022-06-09 11:49:35 +0200

    Expose swagger

--- DISP_Saga/InventoryService/Program.cs
+++ DISP_Saga/InventoryService/Program.cs
@@ -35,11 +35,10 @@ builder.Services.AddSingleton<IReservationRepository, ReservationRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
